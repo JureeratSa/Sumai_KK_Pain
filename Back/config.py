f9f588105895359f6442fb77121f7244a393d0fa
Jureerat Saenchom <jureerat.dev@gmail.com>
2026-02-06 00:04:12 +0700

    Fix absolute path in config.py for deployment

--- Back/config.py
+++ Back/config.py
@@ -4,7 +4,8 @@ import os
 
 def initialize_firebase():
     base_path = os.path.dirname(os.path.abspath(__file__))
-    cred_path = os.path.join(base_path, "/Users/kaew/Downloads/SIC_Sumai_Kaew-main/Back/paindetection-d0ca2-firebase-adminsdk-fbsvc-2da0b73237.json")
+    cred_filename = "paindetection-d0ca2-firebase-adminsdk-fbsvc-2da0b73237.json"
+    cred_path = os.path.join(base_path, cred_filename)
     cred = credentials.Certificate(cred_path)
     firebase_admin.initialize_app(cred, {
         "databaseURL": "https://paindetection-d0ca2-default-rtdb.asia-southeast1.firebasedatabase.app/"
